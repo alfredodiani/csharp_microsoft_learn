4a893a6b381ff4b7a828ccddd78a9d69492e46ca
Alfredo Diani <alfredodiani@hotmail.com>
2023-08-06 03:02:47 -0300

    13_arrays desafio 2

---
+++ 13_arrays_metodos_auxiliares_desafio_2/Program.cs
@@ -0,0 +1,16 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderArray = orderStream.Split(',');
+Array.Sort(orderArray);
+
+foreach (var order in orderArray)
+{
+    if(order.Length == 4)
+    {
+        Console.WriteLine(order);
+    }
+    else
+    {
+        Console.WriteLine(order + "\t- Error");
+    }
+}
