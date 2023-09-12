b3a6203adf77d7d97936621b8590e31540413232
Alfredo Diani <alfredodiani@hotmail.com>
2023-09-12 03:56:47 -0300

    methods with parameters

---
+++ 28_methods_with_parameters_value_reference/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+    Parameters by Value
+*/
+
+int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+void Multiply(int a, int b, int c) 
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
+Console.WriteLine();
+
+
+/*
+    Parameters by Reference
+*/
+
+int[] array = {1, 2, 3, 4, 5};
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array) 
+{
+    foreach (int a in array) 
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = 0;
+    }
+}
+ Console.WriteLine("\n");
+
+
+/*
+    Test with strings
+
+    It is important to remember that string is a reference type, but it is immutable. 
+    That means once it has been assigned a value, it can't be altered. In C#, when methods 
+    and operators are used to modify a string, the result that is returned is actually a 
+    new string object.
+*/
+
+ 
+// string status = "Healthy";
+
+// Console.WriteLine($"Start: {status}");
+// SetHealth(status, false);
+// Console.WriteLine($"End: {status}");
+
+// void SetHealth(string status, bool isHealthy) 
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
+
+//-- a new string with the value "Unhealthy" was created and then lost in the method scope
+//-- To correct this problem, you can change SetHealth to use the global status variable instead.
+
+string status = "Healthy";
+
+Console.WriteLine($"Start: {status}");
+SetHealth(false);
+Console.WriteLine($"End: {status}");
+
+void SetHealth(bool isHealthy) 
+{
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
+}
---
+++ 29_methods_named_and_optional_parameters/Program.cs
@@ -0,0 +1,58 @@
+﻿
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+
+/*
+A parameter becomes optional when it's assigned a default value. If an optional parameter is omitted from the arguments, 
+the default value is used when the method executes. In the following method parameters partySize, allergies and inviteOnly are optional.
+*/
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
+
+/*
+Notice that you supply the name of the parameter, followed by a colon and the value. 
+This syntax defines a named argument. It isn't necessary to name all of the arguments. 
+For example, the following syntax is also valid:
+
+*/
+
+RSVP("Rebecca");    //optional parameters are not being omited and receiving the default value
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false); //using named arguments
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true); //using named arguments for ags 2, 3 and 4. arg 1 is defined by placement
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+
+ShowRSVPs();
